afe170e51af56d8193b3435e576ba048282dddd7
Tran Nguyen Dang Khoi <khoi.tran@infodation.local>
2020-05-13 15:09:45 +0700

    push update code

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,50 +1,56 @@
 import React from 'react';
 
-class Game extends React.Component{
-	constructor(props){
-		super(props);
-		this.handleSubmit=this.handleSubmit.bind(this);
-		this.handleChange=this.handleChange.bind(this);
-		this.randomNumber=this.randomNumber.bind(this);
-		this.state=({
-			inputNumber:null,
-		});
-	}
-	randomNumber = () =>{
-		return Math.ceil(Math.random() * 100);
-	}
-	compareToNumber=(number) =>{
-		  // if (number==numberGuess){
-		  // 	console.log("very good");
-		  // 	return number;
-		  // }
-	}
-	handleChange=(e) => {
-		this.setState=({
-			inputNumber:e.target.value
-		});
-		console.log(this.state.inputNumber);
-	}
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.randomNumber = this.randomNumber.bind(this);
+        this.state = ({
+            inputNumber: 0,
+            initialNumber: 0
+        });
+    }
+    randomNumber = () => {
+        return Math.ceil(Math.random() * 100);
+    }
+    compareToNumber = (number, numberGuess) => {
+        if (number == numberGuess) {
+            alert("very good");
+        } else {
+            alert("not bad");
+        }
+    }
+    handleChange = (e) => {
+        this.setState({
+            inputNumber: parseFloat(e.target.value)
+        });
+    }
 
-	handleSubmit=(e)=>{
-		console.log(e.target.name);
-		e.preventDefault();		
-		this.compareToNumber(this.state.inputNumber);
-	}
-	render(){
-		const numberGuess= this.randomNumber();
-		console.log(numberGuess);
-		return(
-			<div className="game_content">
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.initialNumber);
+        console.log(this.state.inputNumber);
+        this.compareToNumber(this.state.inputNumber, this.state.initialNumber);
+    }
+    componentDidMount() {
+        const c_initialNumber = this.randomNumber();
+        this.setState({
+            initialNumber: c_initialNumber
+        })
+    }
+    render() {
+        return (
+            <div className="game_content">
 				<h1>Welcome to Guessing Number Game :)</h1>
 				<h3>Guess the number between 1 and 100. Easy, right?</h3>
 				<form onSubmit={this.handleSubmit}>
-					<input name={numberGuess} value={this.state.inputNumber} onChange={this.handleChange} type="number" /><br />
+					<input value={this.state.inputNumber} onChange={this.handleChange}  /><br />
 					<button type="submit">Guess!</button>
 				</form>
 			</div>
-		);
-	}
+        );
+    }
 }
 
 export default Game;
